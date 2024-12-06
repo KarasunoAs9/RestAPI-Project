25cf059e8f6b375f1cd14a687fe99e9ed72c7193
Dem <demagandrej@gmail.com>
2024-12-06 19:33:47 +0200

    add update and delete method

--- models/event.go
+++ models/event.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/KarasunoAs9/RestAPI-Project/db"
+
 )
 
 
@@ -30,6 +31,37 @@ func (e *Event) Save() error {
 	}
 
 	return err
+}
+
+func (e *Event) UpdateEvent(id, userId int64) error {
+	query := `
+	UPDATE events
+	SET name = $1,
+			description = $2,
+			location = $3
+			WHERE id = $4 and user_id $5
+	`
+
+	row := db.DB.QueryRow(query, e.Name, e.Description, e.Location, id, userId)
+
+	if row == nil {
+		return fmt.Errorf("no such event on database")
+	}
+
+	return nil
+
+}
+
+func (e *Event) DeleteEvent(id, userId int64) error {
+	query := "DELETE FROM events WHERE id = $1 AND user_id = $2"
+
+	_, err := db.DB.Exec(query, id, userId)
+
+	if err != nil {
+		return fmt.Errorf("no such event on database")
+	}
+	
+	return nil
 }
 
 func (e *Event) GetEventById(id int64) error {
